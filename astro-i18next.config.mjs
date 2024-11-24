46f7fb5a41e5c80721963b18c4936b16391b552e
cxy <373052658@qq.com>
2024-11-24 22:29:58 +0800

    back

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-    defaultLanguage: 'zh',
-    supportedLanguages: ['en', 'zh'],
-    i18next: {
-        debug: true,
-        initImmediate: false,
-        fallbackLng: 'zh',
-    },
+    defaultLocale: "zh",
+    locales: ['en', 'zh']
 };
